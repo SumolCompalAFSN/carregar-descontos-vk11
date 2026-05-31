809b1e00483a5670cde8c3dd342c69feddb42d5b
SumolCompalAFSN <pedro.gameiro@sumolcompal.pt>
2026-05-31 02:56:47 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1493,7 +1493,7 @@ export default function App() {
 
       {/* Seller Header */}
      <header className="bg-white border-b border-slate-200 sticky top-0 z-45 px-6 py-4 shadow-xs">
-  <div className="max-w-[1250px] w-full mx-auto flex items-center justify-between gap-4">
+  <div className="max-w-[1230px] w-full mx-auto flex items-center justify-between gap-4">
     
     {/* Lado esquerdo */}
 <div className="flex items-center gap-3 min-w-0">
